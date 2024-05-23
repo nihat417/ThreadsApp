deed15c4364cfad2fad119913771d5019ddfbcca
nihat417 <akreminihat@gmail.com>
2024-05-23 12:53:33 +0400

    v-0.0.1 Added natwind

---
+++ src/common/StyledComponents.jsx
@@ -0,0 +1,10 @@
+import { styled } from "nativewind";
+import {Text, View, Image, ScrollView,
+    TextInput,TouchableOpacity} from 'react-native';
+
+export const StyledView = styled(View);
+export const StyledText = styled(Text);
+export const StyledInput = styled(TextInput);
+export const StyledButton = styled(TouchableOpacity);
+export const StyledScrollView = styled(ScrollView);
+export const StyledImage = styled(Image);
